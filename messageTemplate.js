421bdfd849a05788a4a9ab59af2f3d34c0c59124
shinsuke <matsuda>
2018-09-04 06:59:30 +0000

    update

--- messageTemplate.js
+++ messageTemplate.js
@@ -6,31 +6,31 @@ exports.textMessage = function(text)
   }
 }
 
-exports.imagemapMessage = function (typeName, text) {
+exports.imagemapMessage = function (messages, url) {
   return {
     "type": "imagemap",
-    "baseUrl": 'replace your image url', // input your image path
+    "baseUrl": url, // input your image path
     "altText": "This is an imagemap",
     "baseSize": {"height": 1040,"width": 1040},
     "actions": [
       {
         "type": "message",
-        "text": text[0],
+        "text": messages[0],
         "area": {"x": 0,"y": 360,"width": 520,"height": 340}
       },
       {
         "type": "message",
-        "text": text[1],
+        "text": messages[1],
         "area": {"x": 520,"y": 360,"width": 520,"height": 340}
       },
       {
         "type": "message",
-        "text": text[2],
+        "text": messages[2],
         "area": {"x": 0,"y": 700,"width": 520,"height": 340}
       },
       {
         "type": "message",
-        "text": text[3],
+        "text": messages[3],
         "area": {"x": 520,"y": 700,"width": 520,"height": 340}
       }
     ]
