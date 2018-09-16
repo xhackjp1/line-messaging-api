a1b471e21838347d1f387f5058368a2866b0459b
Your Name <youremail@example.com>
2018-09-16 16:09:30 +0900

    update with shell command

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ app.post('/callback', function(req, res) {
       gnavi.api(req.body, message_text, function (result) {
         var text = result['name'] + "\n" + result['address'];
         sendMessage.send(req, [
-          messageTemplate.textMessage(text);
+          messageTemplate.textMessage(text),
           messageTemplate.imageMessage(result['shop_image1'], result['shop_image1']);
         ]);
         return;
